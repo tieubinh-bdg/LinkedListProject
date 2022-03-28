3b937c0677338c7661915e5e1518ad0c40319930
Nghiêm Tiểu Bình <tieubinh2205@gmail.com>
2022-03-28 17:23:20 +0700

    Fixed

--- LinkedListProject/LinkedList.h
+++ LinkedListProject/LinkedList.h
@@ -2,31 +2,17 @@
 #include <iostream>
 #include "Node.h"
 using namespace std;
-
-
 class LinkedList {
 private:
 	Node* first;
 public:
-	LinkedList() {
-		first = NULL;
-	}
-	~LinkedList() {
-		Node* a = first;
-		while (a != NULL) {
-			Node* temp = a->next;
-			free(a);
-			a = temp;
-		}
-		cout << "Deconstructor Node" << endl;
-		/*delete[] first;*/
-		cout << "Deconstructor LINKEDLIST" << endl;
-	}
-	bool isEmpty();
-	int getCount();
-	void displayList();
-	int getFirst();
-	int getAt(int position);
-	void insertFirst(int iData);
-	void deleteFirst();
+	LinkedList();
+	~LinkedList();
+	bool IsEmpty();
+	int GetCount();
+	void DisplayList();
+	int GetFirst();
+	int GetAt(int position);
+	void InsertFirst(int iData);
+	void DeleteFirst();
 };
--- LinkedListProject/Node.cpp
+++ LinkedListProject/Node.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 using namespace std;
 
+Node::Node() {}
+
+Node::Node(int iData) {
+	this->data = iData;
+}
 void Node::DislayNode() {
 	cout << "{ ";
 	cout << data << " }";
